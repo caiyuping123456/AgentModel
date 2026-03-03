134842aeed7217dec3ce3e3ecad12c1906342c16
caiyuping123456 <761962102@qq.com>
2026-03-03 16:20:20 +0800

    123

---
+++ Langchain4JModel/src/main/java/org/example/langchain4jmodel/Langchain4JModelApplication.java
@@ -0,0 +1,13 @@
+package org.example.langchain4jmodel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Langchain4JModelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Langchain4JModelApplication.class, args);
+    }
+
+}
---
+++ Langchain4JModel/src/test/java/org/example/langchain4jmodel/Langchain4JModelApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.langchain4jmodel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Langchain4JModelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
