518d58994b930354b4805ba5789837ba11f6ce4e
caiyuping123456 <761962102@qq.com>
2026-03-03 16:20:20 +0800

    123

---
+++ src/main/java/org/example/agentmodel/AgentModelApplication.java
@@ -0,0 +1,13 @@
+package org.example.agentmodel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgentModelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AgentModelApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/agentmodel/AgentModelApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.agentmodel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgentModelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
